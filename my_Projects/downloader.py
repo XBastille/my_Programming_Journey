3635876ccec2d48c9169686ff26da08988bfbcd7
XBastille <148121344+XBastille@users.noreply.github.com>
2023-10-20 12:58:45 +0530

    Create downloader.py

---
+++ my_Projects/downloader.py
@@ -0,0 +1,10 @@
+import requests
+from tqdm import tqdm
+r=requests.get("https://www.win-rar.com/fileadmin/winrar-versions/winrar/winrar-x64-624.exe", stream=True) 
+totalExpectedBytes=int(r.headers["content-length"])
+progress_Bar=tqdm(total=totalExpectedBytes, unit="B", unit_scale=True, colour="red")
+with open("winrar223.exe", 'wb') as fd:
+    for chunk in r.iter_content(chunk_size=1024):
+        progress_Bar.update(len(chunk))
+        fd.write(chunk)
+progress_Bar.close()

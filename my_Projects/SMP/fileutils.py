3225a11880d19c190a29434df7e9bc7540534f37
XBastille <148121344+XBastille@users.noreply.github.com>
2024-06-06 12:21:31 +0530

    Update fileutils.py

--- my_Projects/SMP/fileutils.py
+++ my_Projects/SMP/fileutils.py
@@ -181,7 +181,6 @@ class Progbar(object):
 
         self._last_update = now
 
-    # TODO Rename this here and in `update`
     def update_28(self, current):
         numdigits = int(floor(log10(self.target))) + 1
         barstr = '%%%dd/%d [' % (numdigits, self.target)
